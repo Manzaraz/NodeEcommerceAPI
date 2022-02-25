fcbbc8114588b7711044b836f75927980caa4f58
Manzaraz <magnusmanz@gmail.com>
2022-02-25 12:26:48 -0300

    verifyToken update user

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
-const router = require("express").Router();
-const CryptoJs = require("crypto-js");
-const User = require("../models/User");
+const router = require("express").Router(),
+  CryptoJs = require("crypto-js"),
+  jwt = require("jsonwebtoken"),
+  User = require("../models/User");
 
 // REGISTER
 router.post("/register", async (req, res) => {
@@ -27,12 +28,19 @@ router.post("/login", async (req, res) => {
     !user && res.status(401).json("Wrong crenentials: username doesn't match");
 
     const hashedPasword = CryptoJs.AES.decrypt(
-      user.password,
-      process.env.PW_SECRET
-    );
-
-    const dbPassword = hashedPasword.toString(CryptoJs.enc.Utf8);
-    const { password, ...others } = user._doc;
+        user.password,
+        process.env.PW_SECRET
+      ),
+      dbPassword = hashedPasword.toString(CryptoJs.enc.Utf8),
+      accessToken = jwt.sign(
+        {
+          id: user._id,
+          isAdmin: user.isAdmin,
+        },
+        process.env.JWT_SECRET,
+        { expiresIn: "3d" }
+      ),
+      { password, ...others } = user._doc;
 
     req.body.password !== dbPassword
       ? res
@@ -40,10 +48,15 @@ router.post("/login", async (req, res) => {
           .json(
             `Wrong crenentials: password doesn't match for ${req.body.username}`
           )
-      : res.status(200).json({ ...others });
+      : res.status(200).json({ ...others, accessToken });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
 module.exports = router;
+
+// Chris
+// 1234
+//  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyMTYzMDE0NDI3MzNhYjI2ZDkxOGFjOSIsImlzQWRtaW4iOmZhbHNlLCJpYXQiOjE2NDU4MDA5NDgsImV4cCI6MTY0NjA2MDE0OH0.LnIhnUGS6CBfL77cRDDQNZdp3SVrXZ94k5C71QdjinY"
+// }
--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,29 @@
 const router = require("express").Router();
+const cryptoJs = require("crypto-js");
+const User = require("../models/User");
+const { verifyToken, verifyTokenAndAuthorization } = require("./verifyToken");
 
-router.get("/usertest", (req, res) => {
-  res.send("User test successfull");
+// Update
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  if (req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PW_SECRET
+    ).toString();
+  }
+
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post("/userposttest", (req, res) => {
-  const username = req.body.username;
-  console.log(`Your username is: ${username}`);
-});
-
-// lh: 5500/api/user/usertest
 module.exports = router;
