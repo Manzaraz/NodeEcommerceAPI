580c0d9e035b13aad5a634b50628c34de65911a8
Manzaraz <magnusmanz@gmail.com>
2022-02-25 18:22:25 -0300

    get users stats

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const cryptoJs = require("crypto-js");
+const CryptoJS = require("crypto-js");
 const User = require("../models/User");
 const {
   verifyToken,
@@ -52,7 +52,7 @@ router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
   }
 });
 
-// Get all user
+// Get all users
 router.get("/", verifyTokenAndAdmin, async (req, res) => {
   const query = req.query.new;
   try {
@@ -60,6 +60,32 @@ router.get("/", verifyTokenAndAdmin, async (req, res) => {
       ? await User.find().sort({ _id: -1 }).limit(5)
       : await User.find();
     res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Get user stats
+router.get("/stats", verifyTokenAndAdmin, async (req, res) => {
+  const date = new Date();
+  const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
+
+  try {
+    const data = await User.aggregate([
+      { $match: { createdAt: { $gte: lastYear } } },
+      {
+        $project: {
+          month: { $month: "$createdAt" },
+        },
+      },
+      {
+        $group: {
+          _id: "$month",
+          total: { $sum: 1 },
+        },
+      },
+    ]);
+    res.status(200).json(data);
   } catch (err) {
     res.status(500).json(err);
   }
