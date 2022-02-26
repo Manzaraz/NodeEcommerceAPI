546ff435465d5751fe7404c9220c6b00b4f8cff9
Manzaraz <magnusmanz@gmail.com>
2022-02-25 22:27:30 -0300

    routes UpdatedProduct

--- routes/product.js
+++ routes/product.js
@@ -31,15 +31,15 @@ router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
   }
 });
 
-// // Delete
-// router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
-//   try {
-//     await User.findByIdAndDelete(req.params.id);
-//     res.status(200).json("User has been deleted...");
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+// Delete
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json("User has been deleted...");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // // Get user
 // router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
