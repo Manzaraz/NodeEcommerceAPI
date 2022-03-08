a2b5697b67e4c29ba7d794650164e66f50ce06a6
Manzaraz <magnusmanz@gmail.com>
2022-03-07 23:11:13 -0300

    fixing on develpment 01

--- models/Product.js
+++ models/Product.js
@@ -8,55 +8,10 @@ const ProductShema = new mongoose.Schema(
     categories: { type: Array },
     size: { type: Array },
     color: { type: Array },
-    prize: { type: String, required: true },
+    price: { type: Number, required: true },
     inStock: { type: Boolean, default: true },
   },
   { timestamps: true }
 );
 
 module.exports = mongoose.model("Product", ProductShema);
-
-/** Prodct images
- * https://ibb.co/wBjPq5N
-https://ibb.co/JKXGLQ4
-https://ibb.co/SnkfzBS
-https://ibb.co/x2HgDqB
-https://ibb.co/JHxWSgJ
-https://ibb.co/2Yr0PJ3
-https://ibb.co/dmK6j9J
-https://ibb.co/DKfTTw8
-https://ibb.co/94PK2td
-https://ibb.co/1L4B71H
-https://ibb.co/NnGz2BT
-https://ibb.co/Q8r1ZtN
-https://ibb.co/TLPRvc9
-https://ibb.co/WcWprRc
-https://ibb.co/Lp5ZNH2
-https://ibb.co/7nkVbCd
-https://ibb.co/2P4vyd5
-https://ibb.co/TKWRhf1
-https://ibb.co/YNMjP0f
-https://ibb.co/4K6r8G9
-https://ibb.co/J3FdKXS
-https://ibb.co/LPPPT7B
-https://ibb.co/ZVC7qY7
-https://ibb.co/HzFWgnM
-https://ibb.co/pLSYsmb
-https://ibb.co/qRvNBGY
- */
-
-/**
- * 
- const product = [
-   {
-     title: "Capital Polo - Alpinestars Moto",
-     desc: "Polo clásico de piqué de 2 botones con unión de ADN Alpinestars\n Cuello y puños elásticos.\n Etiqueta principal interna y etiqueta de marca externa.\n Logotipo HD en el pecho delantero.\n 60% algodón / 40% poliéster.\n Imágen ilustrativa.",
-     img: "https://i.ibb.co/NFPYtTW/polo-Black02.png",
-     categories: ["t-shirt", "polo", "man", "summer"],
-     size: ["s", "m", "l", "xl"],
-     color: ["black", "gray", "red", "blue"],
-     prize: 9000,
-     inStock: true,
-    },
-  ];
-  */
--- routes/product.js
+++ routes/product.js
@@ -61,12 +61,17 @@ router.get("/", async (req, res) => {
     let products;
 
     if (qNew) {
-      products = await Product.find().sort({ _id: -1 }).limit(5);
+      products = await Product.find().sort({ createdAt: -1 }).limit(1);
     } else if (qCategory) {
-      products = await Product.find({ categories: { $in: [qCategory] } });
+      products = await Product.find({
+        categories: {
+          $in: [qCategory],
+        },
+      });
     } else {
       products = await Product.find();
     }
+
     res.status(200).json(products);
   } catch (err) {
     res.status(500).json(err);
